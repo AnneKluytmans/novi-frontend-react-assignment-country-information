a3da27b927d71eaaae389d3f604e4afd30127713
AnneKluytmans <annekluytmans1@gmail.com>
2024-06-05 15:01:25 +0200

    Finished assignment 1: fetch all countries

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,33 @@ import axios from 'axios';
 import Header from "./components/Header/Header.jsx";
 import Button from "./components/Button/Button.jsx";
 import Footer from "./components/Footer/Footer.jsx";
+import InputField from "./components/InputField/InputField.jsx";
 import spinningGlobe from './assets/spinning-globe.gif';
 import map from './assets/world_map.png';
 import './App.css';
-import InputField from "./components/InputField/InputField.jsx";
-
+import getRegionClassName from "./helpers/getRegionClassName.js";
 
 
 function App() {
-    function fetchCountries() {
-        return console.log("fetchCountries");
+    const [countries, setCountries] = useState([]);
+    const [error, setError] = useState('');
+
+    async function fetchCountries() {
+        setError('');
+
+        try {
+            const response = await axios.get('https://restcountries.com/v3.1/all');
+            console.log(response.data);
+
+            response.data.sort((a, b) => {
+                return a.population - b.population;
+            });
+
+            setCountries(response.data);
+        } catch (e) {
+            console.error(e);
+            setError('Something went wrong with fetching the countries overview data. Try again later.');
+        }
     }
 
     return (
@@ -23,9 +40,25 @@ function App() {
                     <h2>World regions</h2>
                     <img src={map} alt="Worldmap" className="world-map__img"/>
                 </section>
-                <section className="all-country__section">
+                <section className="countries-overview__section">
                     <h2>Countries overview</h2>
-                    <Button type="button" clickHandler={fetchCountries} className="button" text="Fetch all countries"/>
+                    {countries.length > 0
+                        ? <ul className="countries-overview__list">
+                            {countries.map((country) => {
+                                return (
+                                    <li key={country.name.common}>
+                                        <div className="flag-name__container">
+                                            <img src={country.flags.svg} alt={`Flag off ${country.name.common}`} className="flag"/>
+                                            <h3 className={getRegionClassName(country.region)}>{country.name.common}</h3>
+                                        </div>
+                                        <p className="population">Has a population of {country.population} people</p>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                        : <Button type="button" clickHandler={fetchCountries} className="button" text="Fetch all countries"/>
+                    }
+                    {error && <p>{error}</p>}
                 </section>
                 <section className="search-country__section">
                     <h2>Search country information</h2>
